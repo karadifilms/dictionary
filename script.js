3ecd0eb683a0873e5f065656f5d1e8339759c8e2
karadifilms <263832039+karadifilms@users.noreply.github.com>
2026-09-03 21:32:34 +0530

    feat(render): display all words from a single day together

--- script.js
+++ script.js
@@ -12,23 +12,36 @@ async function loadDictionary() {
 
         dictionaryContainer.innerHTML = "";
 
-        words.forEach(word => {
-            const wordElement = document.createElement("div");
-            wordElement.className = "word";
+        const byDate = words.reduce((groups, word) => {
+            (groups[word.date_added] = groups[word.date_added] || []).push(word);
+            return groups;
+        }, {});
 
-            wordElement.innerHTML = `
-                <div class="word-row">
-                    <div class="kannada">${word.kannada}</div>
-                    <div class="tulu">${word.tulu}</div>
-                    <div class="english">${word.english}</div>
-                </div>
+        Object.entries(byDate).forEach(([date, group]) => {
+            const groupElement = document.createElement("div");
+            groupElement.className = "day-group";
 
-                <div class="date">
-                    Added: ${word.date_added}
-                </div>
-            `;
+            const dateHeader = document.createElement("div");
+            dateHeader.className = "day-date";
+            dateHeader.textContent = date;
+            groupElement.appendChild(dateHeader);
 
-            dictionaryContainer.appendChild(wordElement);
+            group.forEach(word => {
+                const wordElement = document.createElement("div");
+                wordElement.className = "word";
+
+                wordElement.innerHTML = `
+                    <div class="word-row">
+                        <div class="kannada">${word.kannada}</div>
+                        <div class="tulu">${word.tulu.join(", ")}</div>
+                        <div class="english">${word.english}</div>
+                    </div>
+                `;
+
+                groupElement.appendChild(wordElement);
+            });
+
+            dictionaryContainer.appendChild(groupElement);
         });
 
     } catch (error) {
